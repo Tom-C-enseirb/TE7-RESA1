5137cfda75025e4a56801917fd2cd57567e70e5d
CARON Tom <Tom.Caron@bordeaux-inp.fr>
2026-09-22 15:49:37 +0200

    req1.1 et 1.2

--- sample-jalon1/client.c
+++ sample-jalon1/client.c
@@ -34,37 +34,51 @@ void echo_client(int sockfd) {
 	}
 }
 
-int handle_connect() {
+int handle_connect(const char *server_name, const char *server_port) {
+	
 	struct addrinfo hints, *result, *rp;
+	
 	int sfd;
+	
 	memset(&hints, 0, sizeof(struct addrinfo));
+	
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
-	if (getaddrinfo(SERV_ADDR, SERV_PORT, &hints, &result) != 0) {
+	
+	if (getaddrinfo(server_name, server_port, &hints, &result) != 0) {
 		perror("getaddrinfo()");
 		exit(EXIT_FAILURE);
 	}
+	
 	for (rp = result; rp != NULL; rp = rp->ai_next) {
 		sfd = socket(rp->ai_family, rp->ai_socktype,rp->ai_protocol);
+		
 		if (sfd == -1) {
 			continue;
 		}
+		
 		if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1) {
 			break;
 		}
 		close(sfd);
 	}
+
 	if (rp == NULL) {
 		fprintf(stderr, "Could not connect\n");
 		exit(EXIT_FAILURE);
 	}
+	
 	freeaddrinfo(result);
 	return sfd;
 }
 
-int main() {
+int main(int argc, char** argv) {
 	int sfd;
-	sfd = handle_connect();
+	if(argc != 3 ){
+		fprintf(stderr, "Pas le bon nombre d'arguments \n");
+		exit(EXIT_FAILURE);
+	}
+	sfd = handle_connect(argv[1], argv[2]);
 	echo_client(sfd);
 	close(sfd);
 	return EXIT_SUCCESS;
